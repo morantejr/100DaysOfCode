253b4ca34a0c6c16552269eb36fdaa1bcb690cae
Joseph Morante <35009526+morantejr@users.noreply.github.com>
2022-01-02 23:20:53 -0500

    Update tip_calculator.py

--- Day 02/tip_calculator.py
+++ Day 02/tip_calculator.py
@@ -1,1 +1,19 @@
+def tipCalc():
+    bill = input("What was the total bill:\n $")
 
+    FLOAT_BILL = float(bill)
+
+    tip = input("What percentage tip would you like to give? 10, 12, 15?\n")
+
+    FLOAT_TIP = float(tip) /100
+
+    num_people = input("How many people are there to split?\n")
+
+    INT_PEOPLE = int(num_people)
+
+    bill_tip = FLOAT_BILL + (FLOAT_BILL * FLOAT_TIP)
+
+    final = bill_tip / INT_PEOPLE
+
+    print(f"Each person should pay: ${round(final,2)}")
+tipCalc()
